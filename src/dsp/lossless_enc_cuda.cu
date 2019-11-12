b7830dea8a996ee71debcaf7e620b1aecc403250
Kevin Geng <khg@andrew.cmu.edu>
2019-11-12 01:02:59 -0500

    Add the CUDA kernel for lossless green

--- src/dsp/lossless_enc_cuda.cu
+++ src/dsp/lossless_enc_cuda.cu
@@ -24,15 +24,41 @@
 //------------------------------------------------------------------------------
 // Subtract-Green Transform
 
+
+__global__ void VP8LSubtractGreenFromBlueAndRed_kernel(uint32_t* argb_data, int num_pixels) {
+  // Overall index from position of thread in current block, and given the block we are in.
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  if (index >= num_pixels) return;    // loop guard: for 0 <= i < num_pixels
+
+  const int argb = argb_data[index];
+  const int green = (argb >> 8) & 0xff;
+  const uint32_t new_r = (((argb >> 16) & 0xff) - green) & 0xff;
+  const uint32_t new_b = (((argb >>  0) & 0xff) - green) & 0xff;
+  argb_data[index] = (argb & 0xff00ff00u) | (new_r << 16) | new_b;
+}
+
 static void SubtractGreenFromBlueAndRed_CUDA(uint32_t* argb_data,
                                              int num_pixels) {
 
   printf("HELLO WORLD WE ARE CALLING CUDA FUNCTION\n");
-  int i = 0;
-  // fallthrough and finish off with plain-C
-  if (i != num_pixels) {
-    VP8LSubtractGreenFromBlueAndRed_C(argb_data + i, num_pixels - i);
-  }
+
+  // Compute number of blocks and threads per block
+  const int threadsPerBlock = 512;
+  const int blocks = (num_pixels + threadsPerBlock - 1) / threadsPerBlock;
+
+  // Allocate device memory buffer for result on the GPU
+  uint32_t* result;
+  cudaMalloc(&result, num_pixels * sizeof(uint32_t));
+
+  // Copy input arrays to the GPU using cudaMemcpy
+  cudaMemcpy(result, argb_data, num_pixels * sizeof(uint32_t), cudaMemcpyHostToDevice);
+
+  // Launch kernel to compute VP8LSubtractGreenFromBlueAndRed
+  VP8LSubtractGreenFromBlueAndRed_kernel<<<blocks, threadsPerBlock>>>(result, num_pixels);
+
+  // Copy result from GPU using cudaMemcpy
+  cudaMemcpy(argb_data, result, num_pixels * sizeof(uint32_t), cudaMemcpyDeviceToHost);
+
 }
 
 //------------------------------------------------------------------------------
